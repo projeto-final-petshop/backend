b93155d933f2b4af2dc889284c9ea0aaf6fc7a20
Juliane Maran <julianemaran@gmail.com>
2024-03-24 17:52:36 -0300

    feat: listando utodos os suários cadastrados

--- petconnet/src/main/java/br/com/projetofinal/petconnet/users/service/UserService.java
+++ petconnet/src/main/java/br/com/projetofinal/petconnet/users/service/UserService.java
@@ -10,18 +10,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @AllArgsConstructor
 public class UserService {
 
     private final UserHelper userHelper;
+    private final UserRepository userRepository;
 
     @Transactional
     public UserResponse createUser(UserRequest request) {
         Users users = UserMapper.userMapper().toEntity(request);
         users.setActive(Boolean.TRUE);
         return userHelper.saveUser(users);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponse> getlAllUsers() {
+        List<Users> users = userRepository.findAll();
+        return UserMapper.userMapper().toUserListResponse(users);
     }
 
 }
--- petconnet/src/main/java/br/com/projetofinal/petconnet/users/web/UserController.java
+++ petconnet/src/main/java/br/com/projetofinal/petconnet/users/web/UserController.java
@@ -6,10 +6,9 @@ import br.com.projetofinal.petconnet.users.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -22,6 +21,12 @@ public class UserController {
     public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest request) {
         UserResponse userResponse = userService.createUser(request);
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<UserResponse>> listUsers() {
+        List<UserResponse> userResponses = userService.getlAllUsers();
+        return new ResponseEntity<>(userResponses, HttpStatus.OK);
     }
 
 }
