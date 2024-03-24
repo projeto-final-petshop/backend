df0dfd60a627bf9041bc648f5eea40c20b3c7e40
Juliane Maran <julianemaran@gmail.com>
2024-03-24 16:24:43 -0300

    initial project

---
+++ petconnet/src/main/java/br/com/projetofinal/petconnet/PetconnetApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetofinal.petconnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetconnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetconnetApplication.class, args);
+	}
+
+}
