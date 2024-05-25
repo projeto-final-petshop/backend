3020954c5af3c7ccda251cd7d8d8c678d6c9d555
Juliane Maran <julianemaran@gmail.com>
2024-05-25 15:28:27 -0300

    exclusao e arquivo de log

--- petconnect/src/main/java/br/com/project/petconnect/core/exceptions/generics/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.project.petconnect.core.exceptions.generics;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(Long id, Class<?> entity) {
-        super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist in our records");
-    }
-
-}
